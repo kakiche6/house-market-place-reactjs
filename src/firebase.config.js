f0734f8eb502757a34a6be7cf19683eb2a8d989f
kakiche6 <arezki.inventeam@gmail.com>
2021-12-29 13:58:12 +0100

    first setup, add navbar component setting up firebase

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCWi1tqmlQKHaUbobKEsSST10Xe0UEQuO8',
+  authDomain: 'house-market-place-fe147.firebaseapp.com',
+  projectId: 'house-market-place-fe147',
+  storageBucket: 'house-market-place-fe147.appspot.com',
+  messagingSenderId: '697528672010',
+  appId: '1:697528672010:web:1ff3794eebd586a1d864f3',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
